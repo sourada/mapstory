aa77f6854e5f6e0f39ac4c4765e2cc1ebcdb6ace
Ian Schneider <planablediglet@gmail.com>
2013-04-16 13:31:16 -0600

    add generic action

--- admin.py
+++ admin.py
@@ -3,8 +3,52 @@ from django import forms
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import admin
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.admin import UserAdmin
+import csv
+
+
+def export_as_csv_action(description="Export selected objects as CSV file",
+                         fields=None, exclude=None, query_factory=None):
+    """
+    This function returns an export csv action
+    'fields' and 'exclude' work like in django ModelForm
+    'header' is whether or not to output the column names as the first row
+    """
+    def export_as_csv(modeladmin, request, queryset):
+        """
+        Generic csv export admin action.
+        based on http://djangosnippets.org/snippets/1697/
+
+        queryset is an iterable returning an object
+        with attributes or no-arg callables matching the field names
+        """
+        if query_factory:
+            queryset = query_factory(queryset)
+
+        opts = modeladmin.model._meta
+        field_names = set([field.name for field in opts.fields])
+        if fields:
+            fieldset = set(fields)
+            field_names = field_names & fieldset
+        elif exclude:
+            excludeset = set(exclude)
+            field_names = field_names - excludeset
+
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
+        writer = csv.DictWriter(response, field_names)
+        writer.writeheader()
+
+        for obj in queryset:
+            writer.writerow(dict(zip(field_names,[unicode(getattr(obj, field)).encode("utf-8","replace") for field in field_names])))
+        return response
+    export_as_csv.short_description = description
+    return export_as_csv
+
+admin.site.add_action(export_as_csv_action())
+
 
 class ResourceForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea)
