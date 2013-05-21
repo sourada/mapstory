9002e7264685dadd013d0f016a12974bea476b4e
Ian Schneider <planablediglet@gmail.com>
2013-05-21 15:53:19 -0600

    add activity report, minor rearrange from ba11c86

--- reports/urls.py
+++ reports/urls.py
@@ -5,11 +5,14 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns(
     'mapstory.reports.views',
 
-    url(r"^build-reports",
-        direct_to_template, {"template": "reports/build_reports.html"}),
+    url(r"^$",
+        direct_to_template, {"template": "reports/build_reports.html"},
+        name="reports_list"),
 
-    url(r"^generate-user-report",
+    url(r"generate-user-report",
         'generate_user_report',
-        name="generate_user_report")
+        name="reports_user_report"),
+
+    url(r"activity", "activity_report", name="reports_activity"),
 
 )
--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,7 @@
+from actstream.models import Action
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render_to_response
 from mapstory.admin import export_via_model
 
 
@@ -17,3 +19,15 @@ def generate_user_report(req):
             'id'
         ]
     )
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+def activity_report(req):
+    actions = Action.objects.all().order_by('timestamp')
+    page = int(req.GET.get('page', 0))
+    size = 100
+    count = actions.count()
+    actions = actions[page * size: (page + 1) * size]
+    next_page = page + 1 if count > page + 1 * size else 0
+    return render_to_response('reports/activity.html', {'actions': actions, 'next_page': next_page})
+
