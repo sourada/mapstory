be6d882cfc62eb4284144d9541e3ac0e7695e961
Ivan Willig <iwillig@gmail.com>
2013-05-28 18:17:31 -0400

    Added some test cases and stub functions for our new Protocol

--- media/script/annotations.js
+++ media/script/annotations.js
@@ -1,8 +1,42 @@
 /*jslint browser: true, nomen: true, indent: 4, maxlen: 80 */
-/*global jQuery, OpenLayers */
+/*global Ext, OpenLayers, mapstory */
 
-(function (global, OpenLayers, undefined) {
+(function (global, Ext, OpenLayers, undefined) {
     'use strict';
+    Ext.ns('mapstory.annotations');
+    Ext.ns('mapstory.annotations.protocol');
 
 
-}(window, OpenLayers));
+    mapstory.annotations.Protocol = OpenLayers.Class(OpenLayers.Protocol, {
+
+        initialize: function (options) {
+            this.format = options.format;
+            OpenLayers.Protocol.prototype.initialize.apply(this, arguments);
+
+        },
+
+        destory: function () {
+            OpenLayers.Protocol.prototype.destory.apply(this);
+        },
+
+
+
+        read: function () {
+
+        },
+        /** Takes a an array of features and POSTs then to the correct
+         *  end point
+         *
+         */
+        create: function (features, options) {
+
+        },
+
+        update: function (feature, options) {
+
+        },
+        CLASS_NAME: 'mapstory.annotations.Prototype'
+
+    });
+
+}(window, Ext, OpenLayers));
--- media/script/tests/test.annotations.js
+++ media/script/tests/test.annotations.js
@@ -1,13 +1,57 @@
 /*jslint browser: true, nomen: true, indent: 4, maxlen: 80 */
-/*global jQuery, QUnit, OpenLayers */
+/*global jQuery, QUnit, OpenLayers, mapstory */
 
-(function (Q, undefined) {
+(function (Q, mapstory, undefined) {
     'use strict';
+    var Protocol = mapstory.annotations.Protocol;
 
     Q.module('Mapstory annotations');
 
-    Q.test("Test the OpenLayers protocol", function () {
-        Q.ok(true, 'This test should pass');
+    Q.test('Mapstory annotations', function () {
+
+        var _get, _post, _delete,
+            protocol = new Protocol({
+                url: '/annotations'
+            });
+
+        Q.ok(protocol instanceof Protocol,
+             'Make sure that we don\'t clobber the constructor');
+
+        Q.strictEqual(
+            protocol.url,
+            '/annotations',
+            'Make sure the constructor sets the url correctly'
+        );
+
+        Q.test('test getting/reading the features', function () {
+            // override the default get method on the only OpenLayers
+            // object
+            var pro,
+                _get = OpenLayers.Request.GET; // capture the get
+            // method
+
+            OpenLayers.Request.GET = function () {
+                return 'get';
+            };
+
+            pro = new Protocol({});
+
+            Q.ok(pro.read({}), 'get',
+                 'Reading feature should be handled by Request.GET');
+
+
+            // return the get method to OpenLayers
+            OpenLayers.Request.GET = _get;
+        });
+
+        Q.test('test creating the features', function () {
+            Q.ok(false);
+        });
+
+        Q.test('test deleting features', function () {
+            Q.ok(false);
+        });
+
     });
 
-}(QUnit));
+}(QUnit, mapstory));
