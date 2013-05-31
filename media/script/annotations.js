15d51591c848c16039b974cffcf6b803751e500e
Ivan Willig <iwillig@gmail.com>
2013-05-31 11:44:01 -0400

    Move the openlayers to a javascript file

--- media/script/annotations.js
+++ media/script/annotations.js
@@ -220,4 +220,38 @@
 
     });
 
+    ms.notes.main = function () {
+        var map, layer, bounds;
+
+        map = new OpenLayers.Map({
+            div: 'olMap',
+            layers: [
+                new OpenLayers.Layer.OSM()
+            ]
+        });
+
+        layer = new OpenLayers.Layer.Vector("annotations", {
+            strategies: [new OpenLayers.Strategy.Fixed()],
+            protocol: new ms.notes.Protocol({
+                mapConfig: {
+                    id: 1
+                }
+            })
+        });
+
+        map.addLayers([layer]);
+        bounds = new OpenLayers.Bounds(
+            -79.318576214907,
+            36.508050759734,
+            -68.332248089907,
+            43.099847634734
+        );
+        map.zoomToExtent(bounds.transform(
+            new OpenLayers.Projection('EPSG:4326'),
+            map.getProjection()
+        ));
+
+    };
+
+
 }(window, Ext, OpenLayers));
