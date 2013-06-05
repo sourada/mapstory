f08662202eb197c68af531af9d79da4a7cb2510d
Ian Schneider <planablediglet@gmail.com>
2013-06-05 15:36:41 -0600

    wording on gs announce, follow up to @da60600
    refs #723

--- watchdog/util.py
+++ watchdog/util.py
@@ -6,7 +6,7 @@ def geoserver_trouble_announcement(enable):
     user = User.objects.filter(is_superuser=True)[0]
     ann, created = Announcement.objects.get_or_create(title='Mapping Service Experiencing Heavy Traffic', creator=user)
     if created:
-        ann.content = 'It is likely temporarily down, but should be up soon. If this problem persists please let the administrators know.'
+        ann.content = 'Displaying maps, editing MapStories and uploading StoryLayers may not work properly, but should resume shortly. If this problem persists please let the administrators know.'
         ann.dismissal_type = Announcement.DISMISSAL_NO
         ann.site_wide = True
     if enable:
